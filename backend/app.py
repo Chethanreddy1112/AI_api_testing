f5306df4303ee4b66c6a315385c74492f0802d9d
Chethan Reddy <chethanreddy1112@gmail.com>
2026-06-14 02:19:55 +0530

    Prepare deployment

--- backend/app.py
+++ backend/app.py
@@ -35,7 +35,7 @@ client = OpenAI(
 
 db = mysql.connector.connect(
     host=os.getenv("DB_HOST"),
-    port=int(os.getenv("DB_PORT")),
+    port=int(os.getenv("DB_PORT", 3306)),
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASSWORD"),
     database=os.getenv("DB_NAME")
